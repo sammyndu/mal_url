5bad8ff5b4a87f4fb0944ad4806bace6c799f017
sammyndu <48829214+sammyndu@users.noreply.github.com>
2019-10-18 12:04:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,36 +34,35 @@ url_fields = namespace.model("Url", {"url": fields.String })
 # 		allTokens.remove('com')	#removing .com since it occurs a lot of times and it should not be included in our features
 # 	return allTokens
 
-# def TL():
-# 	urlsdata = './newdata2.csv'	#path to our all urls file
-# 	allurlsdata = pd.read_csv(urlsdata)
+def TL():
+	urlsdata = './newdata2.csv'	#path to our all urls file
+	allurlsdata = pd.read_csv(urlsdata)
 
-# 	allurlsdata = np.array(allurlsdata)	#converting it into an array
-# 	random.shuffle(allurlsdata)	#shuffling
+	allurlsdata = np.array(allurlsdata)	#converting it into an array
+	random.shuffle(allurlsdata)	#shuffling
 
-# 	y = [d[1] for d in allurlsdata]	#all labels 
-# 	urls = [d[0] for d in allurlsdata]	#all urls corresponding to a label (either good or bad)
-# 	vectorizer = TfidfVectorizer(tokenizer=getTokens)	#get a vector for each url but use our customized tokenizer
-# 	X = vectorizer.fit_transform(urls)	#get the X vector
+	y = [d[1] for d in allurlsdata]	#all labels 
+	urls = [d[0] for d in allurlsdata]	#all urls corresponding to a label (either good or bad)
+	vectorizer = TfidfVectorizer(tokenizer=getTokens)	#get a vector for each url but use our customized tokenizer
+	X = vectorizer.fit_transform(urls)	#get the X vector
 
-# 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)	#split into training and testing set 80/20 ratio
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)	#split into training and testing set 80/20 ratio
 
-# 	lgs = LogisticRegression()	#using logistic regression
-# 	lgs.fit(X_train, y_train)
-# 	# print(lgs.score(X_test, y_test))	#pring the score. It comes out to be 98%
+	lgs = LogisticRegression()	#using logistic regression
+	lgs.fit(X_train, y_train)
+	# print(lgs.score(X_test, y_test))	#pring the score. It comes out to be 98%
 # 	joblib.dump(lgs, 'model.joblib')
 # 	joblib.dump(vectorizer, 'vectorizer.joblib', compress=3)
-# 	# return vectorizer, lgs
+	return vectorizer, lgs
 
 # # @app.route('/get/<path:path>')
 # # def show(path):
 # # 	res = getTokens(path)
 # # 	return jsonify(res)
-# # vectorizer, lgs  =
-# TL()
+vectorizer, lgs  = TL()
 
-lgs = joblib.load("model.joblib")
-vectorizer = joblib.load("vectorizer.joblib")
+# lgs = joblib.load("model.joblib")
+# vectorizer = joblib.load("vectorizer.joblib")
 
 @namespace.route('/url')
 class Url(Resource):
